cef466a7ee7c3610ef6b2a62b5f3dad5d07bea39
liza-mae <liza-mae@users.noreply.github.com>
2019-07-07 07:07:21 -0600

    Update config.js

--- x-pack/test/visual_regression/config.js
+++ x-pack/test/visual_regression/config.js
@@ -14,7 +14,7 @@ export default async function ({ readConfigFile }) {
 
     testFiles: [
       require.resolve('./tests/login_page'),
-      require.resolve('./test/canvas')
+      require.resolve('./tests/canvas')
     ],
 
     services: {
