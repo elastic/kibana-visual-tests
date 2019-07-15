fc9d141fc3a04cb5287bdbd550d5ec72ae88c15c
liza-mae <liza-mae@users.noreply.github.com>
2019-07-15 15:42:28 -0600

    Fix typo

--- x-pack/test/visual_regression/config.js
+++ x-pack/test/visual_regression/config.js
@@ -14,7 +14,7 @@ export default async function ({ readConfigFile }) {
 
     testFiles: [
       require.resolve('./tests/login_page'),
-      require.resolve('./tests/canvas')
+      require.resolve('./tests/canvas'),
       require.resolve('./tests/maps')
     ],
 
